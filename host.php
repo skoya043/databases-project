4f5cdc1c2c8397cecb75e11e66e9e6b12637dd0c
kayladonovan <54905167+kayladonovan@users.noreply.github.com>
2020-04-01 00:31:49 -0400

    adding queries v1

--- host.php
+++ host.php
@@ -30,17 +30,60 @@
     dbname=d32q2phg95025m user=lqzdpzojxkruxi password= 5ee44c6c9c16025d9b8e67d6f0e0d182831d9b3c99d5e3e09e96d42f72776b80");
   $stat = pg_connection_status($connection);
 
+  //change to host.id = (user's inputted id) and property.host_id = (user's inputted id)
   $result = pg_query($connection, "SELECT host.id, host.first_name, property.id, property.host_price, property.property_type, property.country FROM host inner join property on host.id = 3000 and property.host_id = 3000");
+  
   if (!$result){
     echo 'error\n';
     exit;
   }
-
-  while ($row = pg_fetch_row($result)){
-    echo "ID: $row[0] NAME: $row[1] PROPERTYID: $row[2] PRICE: $row[3] TYPE: $row[4] COUNTRY: $row[5]";
-    echo "<br />\n";
-  }
-
 ?>
+
+  <table align=center>
+  <table cellspacing="60">
+  <colgroup>
+    <col span="5">
+
+  </colgroup>
+  <tr>
+    <th><p align=center></p></th>
+    <th><p align=center></p></th>
+    <th><p align=center></p></th>
+    <th><p align=center></p></th>
+    <th><p align=center></p></th>
+    <th><p align=center>ID</p></th>
+    <th><p align=center>NAME</p></th>
+    <th><p align=center>PROPERTY ID</p></th>
+    <th><p align=center>PRICE</p></th>
+    <th><p align=center>TYPE</p></th>
+    <th><p align=center>COUNTRY</p></th>
+  </tr>
+
+  </tr>
+  <?php
+    while ($row = pg_fetch_row($result)){
+    echo "<tr>";
+    echo "<td> </td>";
+    echo "<td> </td>";
+    echo "<td> </td>";
+    echo "<td> </td>";
+    echo "<td> </td>";
+    echo "<td> <p align=center>$row[0] </p></td>";
+    echo "<td> <p align=center>$row[1] </p></td>";
+    echo "<td> <p align=center>$row[2] </p></td>";
+    echo "<td> <p align=center>$row[3] </p></td>";
+    echo "<td> <p align=center>$row[4] </p></td>";
+    echo "<td> <p align=center>$row[5] </p></td>";
+    echo "</tr>";
+  }
+  ?>
+</table>
+
+ <!--  while ($row = pg_fetch_row($result)){
+    echo "<p align=center> $row[0] $row[2] $row[3] $row[4] $row[5] ;</p> ";
+    //echo "<br />\n";
+  }
+ -->
+
 </body>
 </html>
