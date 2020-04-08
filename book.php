4ee036bbe40fc7dbb56ee9ad5914f9844df4c5f1
Prash1998 <psivo052@uottawa.ca>
2020-04-08 11:36:17 -0400

    booking and payment

--- book.php
+++ book.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 <body>
+
 <head>
-	 <link href="./style.css" type="text/css" rel="stylesheet">
-</head>	
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+   <link href="./style.css" type="text/css" rel="stylesheet">
+   <script src="script.js"></script>
+</head> 
+
+
+
+<head>
+   <link href="./style.css" type="text/css" rel="stylesheet">
+</head> 
 
 <div>
   <p><a href="./index.html"> Home</a></p>
@@ -12,23 +21,91 @@
 <h2 class='titlecenter'>Book a Property</h2>
 
   <div class="form">
-    <form class="login-form">
-      <input type="text" placeholder="Property ID"/>
-      <input type="text" placeholder="Your Guest ID"/>      
+    <form id="form" name= "form" class="login-form" method="post">
+  <div class="ajax">
+      <input id='propertyID' name='propertyID' type="text" placeholder="Property ID"/>
+      <input  id='guestID' name='guestID' type="text" placeholder="Your Guest ID"/>   
+    
       <p style="margin-top: 10px;" class="checkboxmessage">Check-In Date</p>
-      <input type="date" placeholder="Check-In Date"/>
+      <input id='checkin' name='checkin' type="date" placeholder="Check-In Date"/>
        <p style="margin-top: 10px;" class="checkboxmessage">Check-Out Date</p>
-      <input type="date" placeholder="Check-Out Date"/>
+      <input id='checkout' name='checkout' type="date" placeholder="Check-Out Date"/>
+    
+    
+    <input id="submit" name="submit" type="submit"  value="Submit"/>
+    
+       <input type="button" value="Cancel" onclick="window.location.href='./guest.php'" />
+	   
+	   <input type="button" value="Go To Payment" onclick="window.location.href='./payment.php'" />
 
-      <button>submit</button>
-      <input type="button" value="Go to Payment" onclick="window.location.href='./payment.php'" />
-       <form>
-<input type="button" value="Cancel" onclick="window.location.href='./guestlookup.php'" />
-
-</form>
     </form>
   </div>
 </div>
 
+
+
+
+
+<?php 
+  $connection = pg_connect("host=ec2-3-234-109-123.compute-1.amazonaws.com
+    dbname=d32q2phg95025m user=lqzdpzojxkruxi password= 5ee44c6c9c16025d9b8e67d6f0e0d182831d9b3c99d5e3e09e96d42f72776b80");
+  
+  
+  if(isset($_POST['submit'])){
+	  
+	  
+	  
+
+
+
+    $property_agreementID = rand(8300,9500); 
+	$propertyID=(int) $_POST['propertyID'];
+    $guestID=(int) $_POST['guestID'];
+	$checkin = $_POST['checkin'];
+    $checkout = $_POST['checkout'];
+	
+	
+	$start = DateTime::createFromFormat('Y-m-d', $_POST['checkin']);
+    $end = DateTime::createFromFormat('Y-m-d', $_POST['checkout']);
+
+    if ($start < $end) {
+	
+	$time1=strtotime($checkin);
+    $sday=date('d',$time1);
+    $smonth=date('n',$time1);
+    $syear=date('Y',$time1);
+
+    $time2=strtotime($checkout);
+    $eday=date('d',$time2);
+    $emonth=date('n',$time2);
+    $eyear=date('Y',$time2);
+	
+	}
+	else{
+		
+		  echo "<script type='text/javascript'>alert('Start Date must be before End Date');</script>";
+	}
+	
+
+
+      if($propertyID!=null && $guestID!=null){
+
+    
+    
+    $query = pg_query($connection, "insert into property_agreement values ($property_agreementID, null, $guestID, $propertyID,$sday,$smonth,$syear,$eday,$emonth,$eyear)"); //Insert Query
+    
+    if($query){
+        echo "<script type='text/javascript'>alert('Property Successfully booked');</script>";
+        }
+
+    
+  }else{
+            echo "<script type='text/javascript'>alert('fail');</script>";
+
+  }
+  
+}
+  ?>
 </body>
-</html>
+
+  </html>
--- payment.php
+++ payment.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 <body>
+
 <head>
-	 <link href="./style.css" type="text/css" rel="stylesheet">
-</head>	
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+   <link href="./style.css" type="text/css" rel="stylesheet">
+   <script src="script.js"></script>
+</head> 
+
+
+
+<head>
+   <link href="./style.css" type="text/css" rel="stylesheet">
+</head> 
 
 <div>
   <p><a href="./index.html"> Home</a></p>
@@ -12,18 +21,74 @@
 <h2 class='titlecenter'>Payment</h2>
 
   <div class="form">
-    <form class="login-form">
-      <input type="text" placeholder="Property ID"/>
-      <input type="text" placeholder="Your Guest ID"/>
-      <input type="text" placeholder="Payment Type (Cash, Check, Credit, Debit)"/>
-      <input type="text" placeholder="Amount"/>
-      <button>submit</button>
-  <form>
-<input type="button" value="Cancel" onclick="window.location.href='./guest.php'" />
-</form>
+    <form id="form" name= "form" class="login-form" method="post">
+  <div class="ajax">
+      <input id='propertyID' name='propertyID' type="text" placeholder="Property ID"/>
+      <input  id='guestID' name='guestID' type="text" placeholder="Your Guest ID"/>   
+    
+      <input id='Paytype' name='Paytype' type="text" placeholder="Payment Type (Cash, Check, Credit, Debit)"/>
+      <input id='amountValue' name='amountValue'  type="text" placeholder="Amount"/>
+       
+	   
+    
+    
+    <input id="submit" name="submit" type="submit"  value="Submit"/>
+    
+       <input type="button" value="Cancel" onclick="window.location.href='./guest.php'" />
+	   
+	   
+
     </form>
   </div>
 </div>
 
+
+
+
+
+<?php 
+  $connection = pg_connect("host=ec2-3-234-109-123.compute-1.amazonaws.com
+    dbname=d32q2phg95025m user=lqzdpzojxkruxi password= 5ee44c6c9c16025d9b8e67d6f0e0d182831d9b3c99d5e3e09e96d42f72776b80");
+  
+  
+  if(isset($_POST['submit'])){
+	  
+	  
+
+
+
+    
+	$paymentID = rand(9600,11600);
+	$propertyID=(int) $_POST['propertyID'];
+    $guestID=(int) $_POST['guestID']; 
+	$amountValue=(float) $_POST['amountValue']; 
+	$Paytype=(string) $_POST['Paytype'];
+	
+
+
+      if($propertyID!=null && $guestID!=null  && $amountValue!=null  && $Paytype!=null  ){
+
+    
+    
+    $query = pg_query($connection, "insert into payment values ($paymentID, NULL, NULL, $guestID,$propertyID,NULL,$amountValue,'bought','$Paytype')"); //Insert Query
+	$query2 = pg_query($connection, "Update  payment Set  property_type=(SELECT property_type from property where ID=$propertyID) where ID=$paymentID");
+	$query3 = pg_query($connection, "Update  payment Set  property_agreement_id= (SELECT property_agreement.ID from property_agreement where property_id=$propertyID) where ID=$paymentID");
+	
+	
+	
+    
+    if($query && $query2 && $query3){
+        echo "<script type='text/javascript'>alert('Property Successfully bought');</script>";
+        }
+
+    
+  }else{
+            echo "<script type='text/javascript'>alert('fail');</script>";
+
+  }
+  
+}
+  ?>
 </body>
-</html>
+
+  </html>
